5cdcb76f159685a9288d6902eb496fa11fca73c1
BaharOssareh <78626041+BaharOssareh@users.noreply.github.com>
2023-12-11 09:11:21 -0500

    comment on JS files completed

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-
+// Importing the CLI class
 const CLI = require('./lib/cli.js');
+
+// Creating an instance of the CLI class
 const cli = new CLI();
 
-console.log('Welcome to SVG Logo Maker! 🙂')
+// Displaying a welcome message
+console.log('Welcome to SVG Logo Maker! 🙂');
+
+// Running the CLI application
 cli.run();
